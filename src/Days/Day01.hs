e36b00b4f81b017966b0e455ff838bf50ee510c8
Sam Coy <samcoy3@gmail.com>
2022-12-01 07:18:25 +0000

    Solve day 1

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -1,19 +1,11 @@
 module Days.Day01 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Data.List ( sortOn )
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
+    ( count, sepBy, decimal, endOfLine, Parser )
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +13,19 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = (decimal `sepBy` endOfLine) `sepBy` count 2 endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [[Int]]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = maximum . fmap sum
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = sum . take 3 . sortOn negate . fmap sum
