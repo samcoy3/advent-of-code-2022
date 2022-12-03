11ba841a96b0127f655acb78343be80f70f47f81
Sam Coy <samcoy3@gmail.com>
2022-12-03 10:55:46 +0000

    Solve day 3

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -1,19 +1,13 @@
 module Days.Day03 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
+    ( many1, sepBy, letter, endOfLine, Parser )
+import Control.Applicative (many)
+import Data.Char ( ord, isAlpha, isLower, isUpper )
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +15,44 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many1 letter `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [String]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+-- Gets the priority for letters as defined in the problem
+priority :: Char -> Int
+priority c =
+  if
+      | isAlpha c && isLower c -> ord c - 96
+      | isAlpha c && isUpper c -> ord c - 38
+      | otherwise -> error "Input character was not in a-zA-Z!"
+
+-- Split the input string for each line in half,
+--   then find the character in common between the halves
+--   and sum the priorities
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = sum . fmap (priority . difference . (\s -> splitAt (length s `div` 2) s))
+  where
+    difference (s1, s2) =
+      minimum $
+        Set.intersection (Set.fromList s1) (Set.fromList s2)
 
 ------------ PART B ------------
+-- Recursively take the input lines in threes,
+--   finding the common character and
+--   summing priorities as before
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = getBadges
+  where
+    getBadges [] = 0
+    getBadges (s1 : s2 : s3 : ss) =
+      getBadges ss
+        + ( priority . minimum . foldr1 Set.intersection $
+              [Set.fromList s1, Set.fromList s2, Set.fromList s3]
+          )
