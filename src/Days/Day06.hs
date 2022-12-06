26bdcd96acdf0ba35ceaafaad997a0e23569e491
Sam Coy <samcoy3@gmail.com>
2022-12-06 07:13:15 +0000

    Solve day 6

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -1,19 +1,10 @@
 module Days.Day06 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
+import Data.List ( nub )
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
-import Data.Void
+import Data.Attoparsec.Text ( Parser, many1, letter )
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +12,30 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many1 letter
 
 ------------ TYPES ------------
-type Input = Void
+type Input = String
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+-- Finds a marker (first occurrence of a sequence of distinct characters of a certain size)
+-- Takes a marker length and an input string
+findMarker :: Int -> String -> Int
+findMarker markerLength = findMarker' markerLength markerLength -- We start the string position after the first `markerLength` characters
+  where
+    findMarker' :: Int -> Int -> String -> Int
+    findMarker' markerLength stringPosition string =
+      if (length . nub . take markerLength $ string) == markerLength
+        then stringPosition
+        else findMarker' markerLength (stringPosition + 1) (tail string)
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA = findMarker 4
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = findMarker 14
