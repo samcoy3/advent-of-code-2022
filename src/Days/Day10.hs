0e9c992c48e9cdbff90e660769ebad88d7f67de6
Sam Coy <samcoy3@gmail.com>
2022-12-11 17:15:29 +0000

    Solve day 10

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -1,19 +1,14 @@
 module Days.Day10 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
-import Data.Void
+    ( sepBy, decimal, signed, endOfLine, string, Parser )
+import Control.Applicative ((<|>))
+import Data.Functor (($>))
+import Data.Function ((&))
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -21,19 +16,57 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = instruction `sepBy` endOfLine
+  where
+    instruction =
+      "noop" $> Noop
+        <|> Add <$> (string "addx " >> signed decimal)
 
 ------------ TYPES ------------
-type Input = Void
+data Instruction = Noop | Add Int deriving (Show)
 
-type OutputA = Void
+type Input = [Instruction]
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = CRTScreen
 
 ------------ PART A ------------
+-- Takes a starting value and a list of instructions
+-- Returns the (0-indexed) list of values at each cycle
+executeInstructions :: Int -> [Instruction] -> [Int]
+executeInstructions _ [] = []
+executeInstructions current (i : is) = case i of
+  Noop -> current : executeInstructions current is
+  Add x -> current : current : executeInstructions (current + x) is
+
+-- Computes the list of values at each cycle, finds the cycles we care about,
+--   and computes their total signal score by applying the multipliers and summing up
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA =
+  sum
+    . zipWith (*) [20, 60 ..]
+    . (<$> fmap (flip (!!)) [19, 59 .. 219])
+    . (&)
+    . executeInstructions 1
 
 ------------ PART B ------------
+-- A hack to get around the Show instance for String using surrounding quotes and escaped newlines
+newtype CRTScreen = CRTScreen String
+
+instance Show CRTScreen where
+  show (CRTScreen s) = s
+
+-- Chunks the output values into groups of 40,
+--   then checks to see if the CRT would draw each
+-- It suffices to check whether the cycle number (mod 40) is within 1 of the computed value
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB =
+  CRTScreen
+    . unlines
+    . fmap
+      ( fmap (\p -> if p then '█' else '.') -- U+2588 "Full block"
+          . zipWith (\a b -> abs (a - b) < 2) [0 ..]
+      )
+    . U.chunksOf 40
+    . executeInstructions 1
