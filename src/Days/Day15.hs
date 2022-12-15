0e80402fc878dcea1453961d3ea673dd85b7d0a3
Sam Coy <samcoy3@gmail.com>
2022-12-15 10:07:34 +0000

    Solve part 1 of day 15

--- src/Days/Day15.hs
+++ src/Days/Day15.hs
@@ -14,25 +14,77 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
+import Util.Pair (Pair(..))
+import Data.Ord (comparing)
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
------------- PARSER ------------
-inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
-
 ------------ TYPES ------------
-type Input = Void
+data SensorInfo = SensorInfo
+  { sensor :: Pair Int,
+    nearestBeacon :: Pair Int
+  }
+  deriving (Show)
 
-type OutputA = Void
+type Input = [SensorInfo]
+
+type OutputA = Int
 
 type OutputB = Void
 
+------------ PARSER ------------
+inputParser :: Parser Input
+inputParser = sensorInfo `sepBy` endOfLine
+  where
+    point = do
+      "x="
+      x <- signed decimal
+      ", y="
+      y <- signed decimal
+      return $ Pair (x, y)
+    sensorInfo = do
+      "Sensor at "
+      sensor <- point
+      ": closest beacon is at "
+      nearestBeacon <- point
+      return SensorInfo {..}
+
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+distance :: Pair Int -> Pair Int -> Int
+distance a b = sum . fmap abs $ (-) <$> a <*> b
+
+getExclusionZoneAtYCoord :: Int -> SensorInfo -> Maybe (Int, Int)
+getExclusionZoneAtYCoord y SensorInfo {..} =
+  let distanceFromBeacon = distance sensor nearestBeacon
+      distanceFromYCoord = abs $ y - (snd . getPair $ sensor)
+      sensorX = fst . getPair $ sensor
+   in if distanceFromYCoord > distanceFromBeacon
+        then Nothing
+        else
+          Just
+            ( sensorX - (distanceFromBeacon - distanceFromYCoord),
+              sensorX + (distanceFromBeacon - distanceFromYCoord)
+            )
+
+partA :: Input -> Int
+partA sensorInfos =
+  (+ (-numberOfBeaconsInZone))
+    . sum
+    . fmap (\(a, b) -> b - a + 1)
+    . foldl' combineIntervals []
+    . sortBy (comparing fst)
+    . mapMaybe (getExclusionZoneAtYCoord yCoord)
+    $ sensorInfos
+  where
+    yCoord = 2_000_000
+    numberOfBeaconsInZone = length . filter ((== yCoord) . snd) . nub . fmap (getPair . nearestBeacon) $ sensorInfos
+    combineIntervals [] interval = [interval]
+    combineIntervals intervals@((b, e) : _) (b', e') =
+      if b' <= e
+        then (b, max e e') : tail intervals
+        else (b', e') : intervals
 
 ------------ PART B ------------
 partB :: Input -> OutputB
