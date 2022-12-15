2483013c2b52d88fb865f8f1e3c31a6ec9faa816
Sam Coy <samcoy3@gmail.com>
2022-12-15 18:19:56 +0000

    Rewrite day 15 part 2 (NOT WORKING YET)

--- src/Days/Day15.hs
+++ src/Days/Day15.hs
@@ -14,8 +14,9 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
-import Util.Pair (Pair(..))
+import Util.Pair (Pair(..), (<+>), (<->))
 import Data.Ord (comparing)
+import Data.Ratio ((%))
 {- ORMOLU_ENABLE -}
 
 runDay :: R.Day
@@ -87,11 +88,63 @@ partA sensorInfos =
     $ sensorInfos
   where
     yCoord = 2_000_000
-    numberOfBeaconsInZone = length . filter ((== yCoord) . snd) . nub . fmap (getPair . nearestBeacon) $ sensorInfos
+    numberOfBeaconsInZone =
+      length
+        . filter ((== yCoord) . snd)
+        . nub
+        . fmap (getPair . nearestBeacon)
+        $ sensorInfos
 
 ------------ PART B ------------
-partB :: Input -> Int
+sensorToDiamondBoundaries :: SensorInfo -> ([(Pair Int, Pair Int)], [(Pair Int, Pair Int)])
+sensorToDiamondBoundaries SensorInfo {..} =
+  let [top, left, bottom, right] =
+        (<+> sensor) . Pair
+          <$> [ (0, distanceFromBeacon + 1),
+                (-(distanceFromBeacon + 1), 0),
+                (0, -(distanceFromBeacon + 1)),
+                (distanceFromBeacon + 1, 0)
+              ]
+   in ([(left, top), (bottom, right)], [(top, right), (left, bottom)])
+
+overlaps :: (Pair Int, Pair Int) -> (Pair Int, Pair Int) -> Maybe (Pair Int, Pair Int)
+overlaps l1@(p11, p12) l2@(p21, p22) =
+  if
+      | l1 == l2 -> Nothing
+      | p12 == p21 || p11 == p22 || p11 == p21 || p12 == p22 -> Nothing
+      | p12 `liesOn` l2 && p11 `liesOn` l2 -> Just l1
+      | p12 `liesOn` l2 -> Just (p21, p12)
+      | otherwise -> Nothing
+
+liesOn :: Pair Int -> (Pair Int, Pair Int) -> Bool
+liesOn p l@(q1, q2) =
+  let dist a b = (\(Pair (x, y)) -> sqrt $ fromIntegral x ** 2 + fromIntegral y ** 2) $ a <-> b
+   in dist p q1 + dist p q2 == dist q1 q2
+
+intersects :: (Pair Int, Pair Int) -> (Pair Int, Pair Int) -> Maybe (Pair Int)
+intersects l1@(p1, p2) l2@(p3, _) =
+  if sum p1 `mod` 2 /= sum p3 `mod` 2
+    then Nothing
+    else
+      let delta = (sum p3 - sum p1) `div` 2
+          p = p1 <+> Pair (delta, delta)
+       in if p `liesOn` l1 && p `liesOn` l2 then U.traceShowIdWithContext (l1, l2) $ Just p else Nothing
+
+partB :: Input -> [Pair Int]
 partB sensorInfos =
+  let (forwardBoundaries', backwardBoundaries') = unzip $ sensorToDiamondBoundaries <$> sensorInfos
+      forwardBoundaries = foldr1 (++) forwardBoundaries'
+      backwardBoundaries = foldr1 (++) backwardBoundaries'
+      overlappingForward = catMaybes $ overlaps <$> forwardBoundaries <*> forwardBoundaries
+      overlappingBackward = catMaybes $ overlaps <$> backwardBoundaries <*> backwardBoundaries
+      (Pair (x, y)) = head . catMaybes $ intersects <$> overlappingForward <*> overlappingBackward
+   in -- (4_000_000 * x) + y
+      catMaybes $ intersects <$> overlappingForward <*> overlappingBackward
+
+--   (sensorToDiamondBoundaries (head sensorInfos), head sensorInfos)
+
+partB' :: Input -> Int
+partB' sensorInfos =
   (\((x, _) : _, y) -> ((x - 1) * 4_000_000) + y)
     . head
     . filter ((/= 1) . length . fst)
